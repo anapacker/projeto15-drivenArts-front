f706927aaf5938f74db2139f0cdd41bceaf6ab01
anapacker <anaamaria.packer@gmail.com>
2023-07-13 01:29:01 -0300

    style/tela-login

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import ResetStyle from '../src/styles/ResetStyle.js'
+import ResetStyle from './styles/ResetStyle.js'
+import GlobalStyle from './styles/GlobalStyle.js'
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-
+    <GlobalStyle />
     <ResetStyle />
     <App />
   </React.StrictMode>,
--- src/pages/SigninPage.jsx
+++ src/pages/SigninPage.jsx
@@ -1,3 +1,31 @@
-export default function SigninPage(){
-    return <><h1>Página de login:</h1></>
+import { Link } from "react-router-dom"
+import styled from "styled-components"
+import DrivenArtsLogo from "../components/DrivenArtsLogo"
+
+export default function SigninPage() {
+    return (
+        <SingInContainer>
+            <DrivenArtsLogo />
+            <form>
+                <input placeholder="email" type="email" />
+                <input placeholder="senha" type="password" />
+
+                <button>Entrar</button>
+            </form>
+
+            <Link to="/cadastro">
+                Primeira vez? Cadastre-se!
+            </Link>
+        </SingInContainer>
+    )
 }
+
+const SingInContainer = styled.section`
+  background: linear-gradient(to left, #f0a9d1, #fb9a61);  background: linear-gradient(to left, #f0a9d1, #fb9a61);
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+`
